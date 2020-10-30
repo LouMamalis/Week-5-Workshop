9312ef072d15ef325511aad56aa48c6c064187ee
Lou Mamalis <lcm566@york.ac.uk>
2020-10-30 12:40:45 +0000

    added some comments to my script for task 1 now understand that a bit better as well! PHEW

--- week5-work.R
+++ week5-work.R
@@ -1,26 +1,28 @@
 library(tidyverse)
 
-file <- "data-raw/Human-development-index.csv"
+file <- "data-raw/Human-development-index.csv" #give the dataset the name file, creates a value 
 
-#HDI <- read_csv(file)
+hdi <- read_csv(file) %>% #name the file and read in the data to the environment 
+  janitor::clean_names() #here the data is super messy, 212 obs and 32 variables!! ARGH! 
+#the years are currently columns that we dont want...  
 
-hdi <- read_csv(file) %>% 
-  janitor::clean_names()
-
-hdi <- hdi %>%
+hdi <- hdi %>% #here we are tidying up the data into four neat columns 
   pivot_longer(names_to = "year",
-               values_to = "index",
+               values_to = "index",  #add in a column index and add values to it 
                cols = -c(hdi_rank_2018, country))
 hdi
 
+#we have some na's in our data (in the index column) that we want to remove
 hdi_no_na <- hdi %>% 
-  filter(!is.na(index)) #gives the Na without the !. so include the !
+  filter(!is.na(index)) #need the ! as it gives the data that doesn't have na's in 
+#without the ! we would be filtering only for the data that has na's 
 
+#there are x's in front of all of years - dumb, lets remove them! 
 hdi_no_na <- hdi_no_na %>%
-  mutate(year =  str_replace(year, "x", "") %>% 
-           as.numeric()) #removes the x from in front of the years
+  mutate(year =  str_replace(year, "x", "") %>% #replace any x's that are found with nothings
+           as.numeric()) #removes the x from in front of the years, they are numeric
 
-hdi_summary <- hdi_no_na %>% 
+hdi_summary <- hdi_no_na %>% #calculating the mean for the index column
   group_by(country) %>% 
   summarise(mean_index = mean(index))
 
@@ -30,12 +32,15 @@ hdi_summary <- hdi_no_na %>%
             n = length(index),
             sd_index = sd(index),     #calculating standard deviation 
             se_index = sd_index/sqrt(n)) #calculating standard error
+#we have added in columns which contain the mean, sd and se of the index values 
 
+#here we want to filter the summary data to get the ten countries with the lowest mean HDI
 hdi_summary_low <- hdi_summary %>% 
   filter(rank(mean_index) < 11)
 
-hdi_summary_low
+hdi_summary_low #this new data set just contains the info for the ten countries with the lowest mean HDI
 
+#we are now going to make a plot of these ten countries
 hdi_summary_low %>% 
   ggplot() +
   geom_point(aes(x = country,
@@ -66,42 +71,13 @@ hdi_summary_low %>%
   theme_classic() +
   coord_flip()
 
-#create a plot for hdi dataset
+#create a plot for the hdi dataset that we created at the beginning with 6360 obs
 #without creating any intermediate data structures
 #have no clue!!!!
 #will need to try again! 
 
-hdi %>% 
-  ggplot() +
-  geom_point(aes(x = country,
-                 y = index)) +
-  geom_errorbar(aes(x = country,
-                    ymin = index - index,
-                    ymax = index + index)) +
-  scale_y_continuous(limits = c(0, 0.5),
-                     expand = c(0, 0),
-                     name = "HDI") +
-  scale_x_discrete(expand = c(0, 0),
-                   name = "") +
-  theme_classic() +
-  coord_flip()
 
-hdi_summary_low %>% 
-  ggplot() +
-  geom_point(aes(x = country,
-                 y = mean_index)) +
-  geom_errorbar(aes(x = country,
-                    ymin = mean_index - se_index,
-                    ymax = mean_index + se_index)) +
-  scale_y_continuous(limits = c(0, 0.5),
-                     expand = c(0, 0),
-                     name = "HDI") +
-  scale_x_discrete(expand = c(0, 0),
-                   name = "") +
-  theme_classic() +
-  coord_flip()
-
-
+##TASK 2##
 #####data from buoy#####
 file <- "http://www.ndbc.noaa.gov/view_text_file.php?filename=44025h2011.txt.gz&dir=data/historical/stdmet/"
 readLines(file, n = 4) #read in the data and we can look at the first few lines so that we can see what type of data this is 
