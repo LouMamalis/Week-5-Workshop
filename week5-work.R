05193786bd81429f7a4589e56cd7052403d0397f
Lou Mamalis <lcm566@york.ac.uk>
2020-10-30 12:27:23 +0000

    added explanatory comments to task 2,2 now understand it!!

--- week5-work.R
+++ week5-work.R
@@ -105,27 +105,37 @@ hdi_summary_low %>%
 #####data from buoy#####
 file <- "http://www.ndbc.noaa.gov/view_text_file.php?filename=44025h2011.txt.gz&dir=data/historical/stdmet/"
 readLines(file, n = 4) #read in the data and we can look at the first few lines so that we can see what type of data this is 
+#first two rows contain the measure and the units of the data 
 
 #data starts on line 3, so when we read it in we need to skip the first two rows
 
 buoy44025 <- read_table(file,
-                        col_names = FALSE, #?? 
+                        col_names = FALSE, #the names of cols are not correct?
                         skip = 2)
 #strict way of reading in data. each line needs to be the same length and each field and in the same position in every line.
 
-measure <- scan(file,
-                nlines = 1,
+#measure is a vector that contains the new column names that you want to paste into the buoy44025
+#these come from the original 'file' that we wrote in at the beginning
+#we want to extract and edit them so we can use them in the new buoy44025 dataset
+#saves them as characters
+measure <- scan(file, 
+                nlines = 1, #one line of data to be used
                 what = character()) %>%
-  str_remove("#")
+  str_remove("#") #removes the hashtags that are at the start of each row
 
+#units is also a vector that comes from the original 'file' that we read in at the beginning
+#units that the measurements are taken in 
+#they are listed in the second line of data so we want to skip the first row 
 units <- scan(file,
-              skip = 1,
-              nlines = 1,
-              what = character()) %>%
-  str_remove("#") %>%
-  str_replace("/", "_per_")
+              skip = 1, 
+              nlines = 1, #one line of data to be used
+              what = character()) %>% #character is the type of data to be read
+  str_remove("#") %>% #again remove the hashtag at the start
+  str_replace("/", "_per_") #replacing them with / in the data and _per_ to display the units correctly
 
-names(buoy44025) <- paste(measure, units, sep = "_")
+names(buoy44025) <- paste(measure, units, sep = "_") 
 names(buoy44025)
+#now we are pasting in the measure and units that we extracted from 'file' and putting them 
 #change the names of the columns and replace them with new names
+#sep is used to denote the symbol that goes between measure and unit when they are pasted in
 
